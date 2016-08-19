85f24468de28ca4ce192fc0e846752ec2ea6b10b
vab9 <vbrinkhege@uos.de>
2016-08-19 16:55:57 +0200

    add a basic drag and drop game to 3.4.5 (WIP)

---
+++ js/drag-drop.js
@@ -0,0 +1,44 @@
+
+function dragenter(ev) {
+    ev.target.style.opacity = 0.4;
+}
+
+function dragleave(ev) {
+    ev.target.style.opacity = 1.0;
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function dragstart(ev) {
+  ev.dataTransfer.setData("id", ev.target.id);
+  ev.dataTransfer.setData("text", ev.target.classList[1]);
+}
+
+function drop(ev) {
+    dragleave(ev);
+    ev.preventDefault();
+
+    var element_id = ev.dataTransfer.getData("id");
+    var classname = ev.dataTransfer.getData("text");
+
+    if (ev.target.classList.contains(classname)) {
+        // make sure we append to target and not a measure already inside it
+        if (ev.target.classList.contains("target")) {
+            ev.target.appendChild(document.getElementById(element_id));
+        } else {
+            ev.target.parentNode.appendChild(document.getElementById(element_id));
+        }
+    }
+
+    // check if game complete
+    var left_column = document.getElementById("left_column");
+    if (left_column.childElementCount == 0) {
+        console.log("YESSA");
+        var newChild = document.createTextNode("Super! Alle Maßnahmen wurden richtig zugeordnet!");
+        left_column.appendChild(newChild);
+    }
+}
+
+console.log("drag-drop loaded");

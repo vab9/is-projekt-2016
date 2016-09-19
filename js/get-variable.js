50dc94d7bd2bb77bbfc9935779ba37eeafff3800
vab9 <vbrinkhege@uos.de>
2016-09-19 17:19:28 +0200

    add script to access input fields from twine

---
+++ js/get-variable.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function submitVariable(input) {
+    //Get the value of the textbox at time of click
+    var newVariable = $("input[name='" + input + "']")[0].value;
+    //Find the hook node based on name and set the text inside
+    $("tw-hook[name*='" + input + "']").text(newVariable);
+    //Log the Change
+    console.log(input + " changed.")
+}

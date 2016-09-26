3078fbd9fbe006e7cdda8198c4a2970128b181aa
vab9 <vbrinkhege@uos.de>
2016-09-27 00:05:32 +0200

    reorganize folders again

--- server.py
+++ server.py
@@ -4,9 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'HELP! !'
+# @app.route('/')
+# def index():
+#     return 'HELP! !'
 
 @app.route('/kaum')
 def test():

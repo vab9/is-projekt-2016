44c5728e1d7925c9d8372c4cb656940dd1b8d476
vab9 <vbrinkhege@uos.de>
2016-09-27 17:17:12 +0200

    cleanup server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from flask import Flask, send_file, redirect, url_for
+from flask import Flask, send_file
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
@@ -12,29 +12,7 @@ def index():
     return send_file(open('index.html'))
 
 
-@app.route('/res/<path>')
-def media_resources(path):
-    return redirect(url_for('static', filename='res/' + path))
-
-
 @app.route('/save-score/<player>')
 def save_score(player):
     return "Hello My Friend!"
     # find player score, compare to database, save etc.
-
-
-
-
-# import http.server
-# import socketserver
-
-# PORT = 8000
-
-# Handler = http.server.SimpleHTTPRequestHandler
-
-# httpd = socketserver.TCPServer(("127.0.0.1", PORT), Handler)
-
-# print("serving at port", PORT)
-# httpd.serve_forever()
-
-# python3 -m http.server -b 127.0.0.1

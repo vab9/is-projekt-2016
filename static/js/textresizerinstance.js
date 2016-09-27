db8e7dd1045d3ba61b290bb98851b97a00a16175
vab9 <vbrinkhege@uos.de>
2016-09-27 20:36:21 +0200

    text doesn't collapse anymore -- more work needed

---
+++ static/js/textresizerinstance.js
@@ -0,0 +1,9 @@
+var mytextsizer=new fluidtextresizer({
+    controlsdiv: "", //id of special div containing your resize controls. Enter "" if not defined
+    targets: ["tw-passage"], //target elements to resize text within: ["selector1", "selector2", etc]
+    levels: 3, //number of levels users can magnify (or shrink) text
+    persist: "session", //enter "session" or "none"
+    animate: 200 //animation duration of text resize. Enter 0 to disable
+});
+
+// inverse css selector - so only passage text is magnified

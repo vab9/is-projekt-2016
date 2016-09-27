548354340dc4ac92fa22b8dd9388128e96d1b4fb
vab9 <vbrinkhege@uos.de>
2016-09-27 02:08:38 +0200

    added special redirect for media resources

--- server.py
+++ server.py
@@ -1,19 +1,26 @@
 #! /usr/bin/env python
 
-from flask import Flask, request, send_file
+from flask import Flask, send_file, redirect, url_for
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
-# root should really point to a sub-directory. This is a security hole
 noiseapp = WhiteNoise(app, root='./static/')
+
 
 @app.route('/')
 def index():
     return send_file(open('index.html'))
 
-@app.route('/kaum')
-def test():
+
+@app.route('/res/<path>')
+def media_resources(path):
+    return redirect(url_for('static', filename='res/' + path))
+
+
+@app.route('/save-score/<player>')
+def save_score(player):
     return "Hello My Friend!"
+    # find player score, compare to database, save etc.
 
 
 

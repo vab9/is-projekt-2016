3184f569a5f2ed6c4d8b8a45b8f273a50c8d45e7
vab9 <vbrinkhege@uos.de>
2016-09-28 17:11:13 +0200

    rm session textsize save, add flexboxes to header and footer, cleanup style.css

--- static/js/textresizerinstance.js
+++ static/js/textresizerinstance.js
@@ -2,6 +2,6 @@ var mytextsizer=new fluidtextresizer({
     controlsdiv: "", //id of special div containing your resize controls. Enter "" if not defined
     targets: ["tw-passage"], //target elements to resize text within: ["selector1", "selector2", etc]
     levels: 3, //number of levels users can magnify (or shrink) text
-    persist: "session", //enter "session" or "none"
+    persist: "none", //enter "session" or "none"
     animate: 200 //animation duration of text resize. Enter 0 to disable
 });

203660283fe3b3dc5452a0249d83b2ce8ca5673a
vab9 <vbrinkhege@uos.de>
2016-09-29 20:36:08 +0200

    add some things to server.py

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 #! /usr/bin/env python
 
+import os
 from flask import Flask, send_file
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
 noiseapp = WhiteNoise(app, root='./static/')
+
+# Debugging on
+app.debug = True
 
 
 @app.route('/')
@@ -12,7 +16,18 @@ def index():
     return send_file(open('index.html'))
 
 
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello " + name
+
+
 @app.route('/save-score/<player>')
 def save_score(player):
     return "Hello My Friend!"
     # find player score, compare to database, save etc.
+
+
+if 'DATABASE_URL' in os.environ('DATABASE_URL'):
+    db_url = os.environ('DATABASE_URL')
+else:
+    db_url = 'postgres://serious-db'

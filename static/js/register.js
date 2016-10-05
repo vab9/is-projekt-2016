788a2b4485d9726be05597d5051ff16bfeaca675
vab9 <vbrinkhege@uos.de>
2016-10-05 23:27:10 +0200

    put util functions into another .js file

--- static/js/register.js
+++ static/js/register.js
@@ -1,49 +1,5 @@
 "use strict";
 
-function setTwineVariable(name, value) {
-    // get state Object
-    var _harlowe_state = window.harlowe.State;
-    if (typeof(name) === 'undefined' || name === '' || typeof(value) === 'undefined') {
-        // then cannot set variable
-        return;
-    } else if (name[0] === '$') {
-        // remove leading $ if present
-        name = name.slice(1, name.length);
-    }
-    _harlowe_state.variables[name] = value;
-}
-
-function getTwineVariable(name) {
-    // get state Object
-    var _harlowe_state = window.harlowe.State;
-    if (typeof(name) === typeof(undefined) || name === '') {
-        return;
-    } else if (name[0] === '$') {
-        name = name.slice(1, name.length);
-    }
-    return _harlowe_state.variables[name];
-}
-
-function speicherstandLaden(data) {
-    // return true if speicherstand added to localstorage
-    var get_url = '/loadgame/' + data['userid'];
-    var request = $.get(get_url);
-    request.done(function(dt, textStatus, jqXHR) {
-        // OK 200
-        // add to local storage
-        // DOUBLE CHECK THIS!!!
-        var k = dt[0];
-        var v = dt[1];
-        window.localStorage.setItem(k, v);
-        return true;
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        // GET FAILED
-        return false;
-    });
-}
-
-// function speicherstandSchreiben()
-
 function showCustomAlert(alertObj, msg='') {
     var newObj = alertObj.clone();
     newObj.append(msg).appendTo($('#alert-placeholder'));
@@ -60,7 +16,6 @@ $(function() {
             var posting = $.post(action_url, data);
             posting.done(function(dt, textStatus, jqXHR){
                 console.log("successfully posted to database");
-                console.log(dt);
                 var msg = 'score' in dt ? "Willkommen! Du wurdest erfolgreich registriert!" : "Willkommen zurück! Du wurdest erfolgreich angemeldet!";
                 showCustomAlert($('.registration-success'), msg);
 

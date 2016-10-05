a605fe5edd665a20611931c939df04687c1ce940
vab9 <vbrinkhege@uos.de>
2016-10-05 19:52:22 +0200

    rm test.html, merge login and registration

--- serious_game.py
+++ serious_game.py
@@ -6,7 +6,7 @@ import logging
 import os
 import sys
 
-from flask import Flask, send_file, request, json
+from flask import Flask, send_file, request
 from flask_sqlalchemy import SQLAlchemy
 from whitenoise import WhiteNoise
 from dateutil.parser import parse as parse_date
@@ -38,29 +38,7 @@ def index():
     return send_file(open('index.html'))
 
 
-@app.route('/test')
-def test_page():
-    return send_file(open('test.html'))
-
-
-@app.route('login', methods=['POST'])
-def login():
-    try:
-        vorname = request.form['vorname']
-        nachname = request.form['nachname']
-        geb = parse_date(request.form['geburtstag'])
-    except Exception as e:
-        raise e
-        return "Unable to parse login form...", 500
-    requested_usrname = vorname + nachname + unicode(geb.date())
-    usr = User.query.filter_by(User.username == requested_usrname).first_or_404()
-    return usr.make_json_data(), 200
-
-
-
-
-
-@app.route('/register', methods=['POST'])
+@app.route('/anmelden', methods=['POST'])
 def register():
     # should do more validation here!
     try:
@@ -69,17 +47,25 @@ def register():
         geb = parse_date(request.form['geburtstag'])
 
     except Exception as e:
-        # sys.stderr.write(('===========\n', error, '===========\n'))
         raise e
-        return "Unable to parse registration form...", 500
+        return "Unable to parse form...", 500
 
-    try:
-        new_user = User(vorname, nachname, geb)
-        db.session.add(new_user)
-        db.session.commit()
-    except:
-        return "Unable to save " + unicode(new_user) + " to database...", 503
-    return new_user.make_json_data(), 200
+    requested_usrname = vorname + nachname + unicode(geb.date())
+    usr = User.query.filter_by(User.username == requested_usrname).first()
+    if usr is None:
+        # register new user
+        try:
+            new_user = User(vorname, nachname, geb)
+            db.session.add(new_user)
+            db.session.commit()
+        except:
+            return "Unable to save " + unicode(new_user) + " to database...", 503
+        return new_user.make_json_data(), 200
+    else:
+        # return existing user info
+        return usr.make_json_data(), 200
+
+
 
 
 # Do this with username instead, can be computed in JS
